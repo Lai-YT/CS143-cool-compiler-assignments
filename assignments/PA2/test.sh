f85f2dde204ec928b13ae7b61413f560345cb8cf
Lai-YT <381xvmvbib@gmail.com>
2023-01-25 17:27:34 +0800

    Add summarization & remove reference
    The reference is removed since I've made many modifications, and
    the difference is great enough.

--- assignments/PA2/test.sh
+++ assignments/PA2/test.sh
@@ -5,8 +5,6 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NO_COLOR='\033[0m'
 
-# This script refers to https://github.com/afterthat97/cool-compiler/blob/master/assignments/PA2/judge.sh.
-
 # Taking the self-writtens tests file and examples as input, compare the
 # differences between the output of reference lexer and the output of our lexer.
 status=0
@@ -21,6 +19,13 @@ for filename in ./tests/*.cl ../../examples/*.cl; do
     status=1
   fi
 done
+
+echo "--------SUMMARIZE--------"
+if [ "$status" -eq 0 ]; then
+  echo -e "${GREEN}ALL OF THE TESTS PASSED${NO_COLOR}"
+else
+  echo -e "${RED}SOME OF THE TESTS FAILED${NO_COLOR}"
+fi
 
 rm -rf refout myout
 exit $status
