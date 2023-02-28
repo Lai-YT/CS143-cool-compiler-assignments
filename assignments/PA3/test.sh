c1102c67d2241337b542955db2942cdcdd47f896
Lai-YT <381xvmvbib@gmail.com>
2023-02-28 11:03:22 +0800

    Update test script

--- assignments/PA3/test.sh
+++ assignments/PA3/test.sh
@@ -5,14 +5,16 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NO_COLOR='\033[0m'
 
+DIFF_ARGS=(-y --width=60 --suppress-common-lines)
+
 # Taking the self-writtens tests file and examples as input, compare the
 # differences between the output of reference lexer and the output of our lexer.
 status=0
-for filename in ./tests/*.cl ./good.cl ./bad.cl ../../examples/*.cl; do
+for filename in ./tests/*.cl ./{good,bad}.cl ../../examples/*.cl; do
   echo "--------Test using" "$filename" "--------"
   ./refparser "$filename" &>refout
   ./myparser "$filename" &>myout
-  if diff refout myout -y --width=60 --suppress-common-lines; then
+  if diff refout myout "${DIFF_ARGS[@]}"; then
     echo -e "${GREEN}PASSED${NO_COLOR}"
   else
     echo -e "${RED}FAILED${NO_COLOR}"
@@ -21,7 +23,7 @@ for filename in ./tests/*.cl ./good.cl ./bad.cl ../../examples/*.cl; do
 done
 
 echo "--------SUMMARIZE--------"
-if [ "$status" -eq 0 ]; then
+if [[ $status -eq 0 ]]; then
   echo -e "${GREEN}ALL OF THE TESTS PASSED${NO_COLOR}"
 else
   echo -e "${RED}SOME OF THE TESTS FAILED${NO_COLOR}"
