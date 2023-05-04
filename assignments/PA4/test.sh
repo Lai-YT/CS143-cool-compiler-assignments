80da4d4352d797c1c5c736deb15f0e63e49404df
Lai-YT <381xvmvbib@gmail.com>
2023-05-04 19:06:19 +0800

    Run examples as tests

--- assignments/PA4/test.sh
+++ assignments/PA4/test.sh
@@ -10,10 +10,19 @@ DIFF_ARGS=(-y --width=60 --suppress-common-lines)
 # Taking the self-written tests file and examples as input, compare the
 # differences between the output of reference lexer and the output of our lexer.
 status=0
-for filename in ./tests/class/*.cl ./tests/method/*.cl ./tests/*.cl ./{good,bad}.cl; do
+for filename in ./tests/class/*.cl ./tests/method/*.cl ./tests/*.cl ./{good,bad}.cl ../../examples/*.cl; do
   echo "--------Test using" "$filename" "--------"
-  ./refsemant "$filename" &>refout
-  ./mysemant "$filename" &>myout
+  case "$filename" in
+  *_test.cl)
+    # test files needs to be compiled with the test target
+    ./refsemant "$filename" "${filename/_test/}" &>refout
+    ./mysemant "$filename" "${filename/_test/}" &>myout
+    ;;
+  *)
+    ./refsemant "$filename" &>refout
+    ./mysemant "$filename" &>myout
+    ;;
+  esac
   if diff refout myout "${DIFF_ARGS[@]}"; then
     echo -e "${GREEN}PASSED${NO_COLOR}"
   else
