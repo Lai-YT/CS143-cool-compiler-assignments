f38d5f8a128f3584468878821368c12e18af57d8
Lai-YT <381xvmvbib@gmail.com>
2023-07-08 17:32:16 +0800

    Add dispatch information for basic classes

--- assignments/PA5/cgen.cc
+++ assignments/PA5/cgen.cc
@@ -404,7 +404,7 @@ void StringEntry::code_def(ostream& s, int stringclasstag)
 
  /***** Add dispatch information for class String ******/
 
-      s << endl;                                              // dispatch table
+      emit_disptable_ref(Str, s);  s << endl;                 // dispatch table
       s << WORD;  lensym->code_ref(s);  s << endl;            // string length
   emit_string_constant(s,str);                                // ascii string
   s << ALIGN;                                                 // align to word
@@ -446,7 +446,7 @@ void IntEntry::code_def(ostream &s, int intclasstag)
 
  /***** Add dispatch information for class Int ******/
 
-      s << endl;                                          // dispatch table
+      emit_disptable_ref(Int, s);  s << endl;             // dispatch table
       s << WORD << str << endl;                           // integer value
 }
 
@@ -490,7 +490,7 @@ void BoolConst::code_def(ostream& s, int boolclasstag)
 
  /***** Add dispatch information for class Bool ******/
 
-      s << endl;                                            // dispatch table
+      emit_disptable_ref(Bool, s);  s << endl;            // dispatch table
       s << WORD << val << endl;                             // value (0 or 1)
 }
 
